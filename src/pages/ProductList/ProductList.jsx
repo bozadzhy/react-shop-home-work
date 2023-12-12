9118326c9593dcb66590f0a8c8ac81f4f9fc5a8d
FedorBoz <bozadzhy.f@gmail.com>
2023-12-12 17:16:31 +0200

    swiper

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -2,12 +2,41 @@ import { useContext } from "react";
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Thumbs,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 import ProductItem from "../../components/ProductItem/ProductItem";
 import ProductsTable from "../../components/ProductsTable/ProductsTable";
 import { NewThemeContext } from "../../context/context";
 
 import "./ProductList.scss";
-
+// const imgArr = [
+//   {
+//     img: "https://images.pexels.com/photos/19328647/pexels-photo-19328647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//   },
+//   {
+//     img: "https://images.pexels.com/photos/19328647/pexels-photo-19328647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//   },
+//   {
+//     img: "https://images.pexels.com/photos/19328647/pexels-photo-19328647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//   },
+//   {
+//     img: "https://images.pexels.com/photos/19328647/pexels-photo-19328647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//   },
+//   {
+//     img: "https://images.pexels.com/photos/19328647/pexels-photo-19328647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//   },
+// ];
 function ProductList() {
   const products = useSelector((state) => state.products);
   const favorites = useSelector((state) => state.favorites);
@@ -17,6 +46,33 @@ function ProductList() {
 
   return (
     <>
+      {/* <div
+        style={{
+         width:"200px",
+          border: "1px solid black",
+          padding: "20px",
+          margin: "100px auto",
+        }}
+      >
+        <Swiper
+          loop={true}
+          spaceBetween={10}
+          slidesPerView={1}
+          navigation={true}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          modules={[Navigation, Thumbs, Pagination, Scrollbar, A11y]}
+        >
+          {imgArr.map((obj, i) => (
+            <SwiperSlide key={i}>
+              <div>
+                <img style={{ width: "200px" }} src={obj.img} alt="img" />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div> */}
+
       {!isTheme ? (
         <div className="container-list">
           {products.map((obj) => (
@@ -35,30 +91,30 @@ function ProductList() {
         </div>
       ) : (
         <>
-        <table>
-          <tbody>
-          <tr style={{backgroundColor: "grey", color: "white"}}>
-            <td>PHOTO</td>
-            <td>PRODUCT NAME</td>
-            <td>PRICE</td>
-            <td>COLOR</td>
-            <td>CART</td>
-            <td>FAVORITES</td>
-          </tr>
-          {products.map((obj) => (
-            <ProductsTable
-              key={obj.id}
-              isFavorite={favorites.some(({ id }) => obj.id === id)}
-              isCart={cart.some(({ id }) => obj.id === id)}
-              name={obj.name}
-              price={obj.price}
-              imgUrl={obj.imgUrl}
-              id={obj.id}
-              color={obj.color}
-            />
-          ))}
-          </tbody>
-        </table>
+          <table>
+            <tbody>
+              <tr style={{ backgroundColor: "grey", color: "white" }}>
+                <td>PHOTO</td>
+                <td>PRODUCT NAME</td>
+                <td>PRICE</td>
+                <td>COLOR</td>
+                <td>CART</td>
+                <td>FAVORITES</td>
+              </tr>
+              {products.map((obj) => (
+                <ProductsTable
+                  key={obj.id}
+                  isFavorite={favorites.some(({ id }) => obj.id === id)}
+                  isCart={cart.some(({ id }) => obj.id === id)}
+                  name={obj.name}
+                  price={obj.price}
+                  imgUrl={obj.imgUrl}
+                  id={obj.id}
+                  color={obj.color}
+                />
+              ))}
+            </tbody>
+          </table>
         </>
       )}
     </>
